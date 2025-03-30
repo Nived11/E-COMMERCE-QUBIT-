7f94ce555f3843acadb8bd4d9d9a3daf71813663
Nived <nivedsuresh5@gmail.com>
2025-03-30 15:06:09 +0530

    99.9%completed ..

--- serverside/server.js
+++ serverside/server.js
@@ -4,23 +4,32 @@ import router from "./router.js";
 import cors from "cors";
 import connection from "./connection.js";
 import bodyParser from "body-parser";
+import path from "path";
 
 env.config();
 
 const app = express();
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
-
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors());
-app.use("/api", router);
+app.use("/api", router); 
+
+// Serve React frontend (Vite `dist` folder)
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 
 connection().then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`server started http://localhost:${process.env.PORT}`);
-    });
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started at http://localhost:${process.env.PORT}`);
+  });
 });
