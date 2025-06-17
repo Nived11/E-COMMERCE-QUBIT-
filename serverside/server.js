f4b4f4c0e7cdbd99bd7e2240f5de134b9f10771d
Nived <nivedsuresh5@gmail.com>
2025-06-17 21:30:54 +0530

    distadded on server

--- serverside/server.js
+++ serverside/server.js
@@ -20,12 +20,12 @@ app.use(cors());
 app.use("/api", router); 
 
 // Serve React frontend (Vite `dist` folder)
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, "../clientside", "dist")));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../clientside", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 
 connection().then(() => {
